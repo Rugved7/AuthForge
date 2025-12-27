d2faa7a56089bbbb4651fd097ba90f4bb3cc59dd
Rugved7 <rugvedagasti7@gmail.com>
2025-12-27 14:12:30 +0530

    feat(auth): tokenManager struct added, func for generating access and refresh token added

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Rugved7/authforge
 go 1.25.5
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.2
 	github.com/google/uuid v1.6.0
 	golang.org/x/crypto v0.46.0
 )
--- internal/auth/token.go
+++ internal/auth/token.go
@@ -1,1 +1,52 @@
+// package auth deals with the authentication and authorization logic
 package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var ErrInvalidToken = errors.New("invalid token")
+
+type TokenManager struct {
+	secret          []byte
+	accessTokenTTL  time.Duration
+	refershTokenTTL time.Duration
+}
+
+func NewTokenManager(secret string, accessTTL, refreshTTL time.Duration) *TokenManager {
+	return &TokenManager{
+		secret:          []byte(secret),
+		accessTokenTTL:  accessTTL,
+		refershTokenTTL: refreshTTL,
+	}
+}
+
+func (tm *TokenManager) GenerateRefreshToken(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"sub":  userID,
+		"exp":  time.Now().Add(tm.refershTokenTTL).Unix(),
+		"iat":  time.Now().Unix(),
+		"type": "refresh",
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(tm.secret)
+}
+
+func (tm *TokenManager) ParseToken(tokenStr string) (*jwt.Token, jwt.MapClaims, error) {
+	claims := jwt.MapClaims{}
+
+	token, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrInvalidToken
+		}
+		return tm.secret, nil
+	})
+	if err != nil || !token.Valid {
+		return nil, nil, ErrInvalidToken
+	}
+	return token, claims, nil
+}
